6affca027abf334dfd3d9e3cd02f190cc3e68ac5
aszostek <01187292@pw.edu.pl>
2024-10-15 22:55:36 +0200

    added plotting in two dimensions and time measurment

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,20 +4,45 @@ import autograd.numpy as np
 from matplotlib import pyplot as plt
 
 
+def init_plot(function):
+    MAX_X = 100
+    PLOT_STEP = 0.1
+
+    x_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
+    y_arr = np.arange(-MAX_X, MAX_X, PLOT_STEP)
+    X, Y = np.meshgrid(x_arr, y_arr)
+    Z = np.empty(X.shape)
+
+    q = function
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            point = np.zeros(10)
+            point[0] = X[i, j]
+            point[1] = Y[i, j]
+            Z[i, j] = q(point)
+
+    plt.contour(X, Y, Z, 20)
+
+
 def quadriatic(x):
     return np.sum(x ** 2)
 
 
-def main():
+def main(function):
 
+    init_plot(function)
     x0 = np.random.uniform(-100, 100, 10)
-    result, iterations, values = solver(f3, x0, 0.000001, 1e-4, 1000, 100)
-    print(result)
-    print(quadriatic(result))
-    plt.plot(range(iterations), values)
+    result, iterations, values, time = solver(function, x0, 0.1, 1e-4, 1000, 100)
     plt.show()
-    print(values)
+    plt.clf()
+    print(np.round(result, decimals=2))
+    print(np.round(function(result), decimals=2))
+    print(f'Time taken: {time:.2f}')
+    plt.plot(range(iterations), values)
+    plt.ylabel('q(xt) - wartość funkcji celu dla iteracji')
+    plt.xlabel('t - numer iteracji')
+    plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main(quadriatic)
--- Lab1/solver.py
+++ Lab1/solver.py
@@ -1,15 +1,26 @@
 from autograd import grad
 import autograd.numpy as np
+from matplotlib import pyplot as plt
+from time import time
 
 
 def solver(f, x, step_size, epsilon, iterations, max_bound):
     gradient_function = grad(f)
     values = []
+    start = time()
     for n in range(iterations):
-        values.append(f(x))
+        previous_x = x
+        y = f(x)
+        values.append(y)
         gradient = gradient_function(x)
         x = x - gradient * step_size
         x = np.clip(x, -max_bound, max_bound)
-        if np.linalg.norm(gradient) <= epsilon:
-            return x, n + 1, values
-    return x, iterations, values
+        plt.arrow(previous_x[0], previous_x[1], x[0] - previous_x[0], x[1] - previous_x[1], head_width=1, head_length=1, fc='k', ec='k')
+        if (
+            (np.linalg.norm(gradient) <= epsilon)
+        ):
+            end = time()
+            return x, n + 1, values, end - start
+    print("Solution not found.")
+    end = time()
+    return x, iterations, values, end - start
