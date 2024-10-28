0fcc90165fa978447a491e5f78ab186ac3cb2edb
Adam Szostek <xadirpl@gmail.com>
2024-10-28 19:27:24 +0100

    fixed errors

--- Lab2/main.py
+++ Lab2/main.py
@@ -7,19 +7,19 @@ from gradient_descent import solver as sgd, SGDParamiters
 from scipy.stats import wilcoxon
 
 
-def my_f3(params):
+def my_f3(params: np.array):
     return f3(params)[0]
 
 
-def my_f7(params):
+def my_f7(params: np.array):
     return f7(params)[0]
 
 
-def quadriatic(x):
+def quadriatic(x: np.array):
     return np.sum(x ** 2)
 
 
-def plot_results(params):
+def plot_results(params: EsStrategyParamiters):
     result, score, all_evals = es(params)
     print(f'Result point X = {np.round(result[0], 8)}')
     print(f'Score: q(X) = {np.round(score, 8)}')
@@ -36,7 +36,7 @@ def plot_results(params):
     plt.xlabel("t - iteration number", fontweight="bold", fontsize=16.0)
 
 
-def plot_mean_results(params, trials):
+def plot_mean_results(params: EsStrategyParamiters, trials: int):
     all_trials = np.zeros((trials, params.max_iterations))
     for trial in range(trials):
         _, _, scores = es(params)
@@ -56,7 +56,7 @@ def plot_mean_results(params, trials):
         label=f'ES(1+1) Mean value from {trials} trials', s=2)
 
 
-def test_wilcoxon(trials: int, es_params: EsStrategyParamiters, grad_params):
+def test_wilcoxon(trials: int, es_params: EsStrategyParamiters, grad_params: SGDParamiters):
     results_sgd = []
     results_es = []
     for _ in range(trials):
@@ -64,7 +64,7 @@ def test_wilcoxon(trials: int, es_params: EsStrategyParamiters, grad_params):
         results_sgd.append(all_values[-1])
         _, result_es, _ = es(es_params)
         results_es.append(result_es)
-        starting_point = np.random.uniform(es_params.max_bound, es_params.max_bound, size=(1, 10))
+        starting_point = np.random.uniform(grad_params.max_bound, grad_params.max_bound, size=(1, 10))
         es_params.starting_point = starting_point
         grad_params.starting_point = starting_point
     statistic, p_value = wilcoxon(results_es, results_sgd)
