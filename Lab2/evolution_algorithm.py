9517e57fd3bf0c49f2088d6962e33fcf995087ff
Adam Szostek <xadirpl@gmail.com>
2024-11-02 22:03:41 +0100

    fixed bug with mutation strength

--- Lab2/evolution_algorithm.py
+++ Lab2/evolution_algorithm.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import Callable
 from dataclasses import dataclass
+from copy import copy
 
 
 @dataclass
@@ -23,11 +24,12 @@ class EsStrategyParamiters:
 def solver(params: EsStrategyParamiters):
     t = 1
     x = params.starting_point
+    mutation_strength = copy(params.mutation_strength)
     success_counter = 0
     current_score = params.function(x)
     scores = []
     while t <= params.max_iterations:
-        mutant = x + params.mutation_strength * np.random.normal(0, 1, len(x[0]))
+        mutant = x + mutation_strength * np.random.normal(0, 1, len(x[0]))
         mutant_score = params.function(mutant)
         if mutant_score <= current_score:
             success_counter += 1
@@ -35,9 +37,9 @@ def solver(params: EsStrategyParamiters):
             x = mutant
         if t % params.adaptaion_interval == 0:
             if success_counter / params.adaptaion_interval > 1 / 5:
-                params.mutation_strength *= 1.22
+                mutation_strength *= 1.22
             if success_counter / params.adaptaion_interval < 1 / 5:
-                params.mutation_strength *= 0.82
+                mutation_strength *= 0.82
             success_counter = 0
         t += 1
         scores.append(current_score)
