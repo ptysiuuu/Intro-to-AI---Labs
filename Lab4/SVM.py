ea93307b244a09cefd8330631014a65f9f26e3fc
aszostek <01187292@pw.edu.pl>
2024-12-10 23:34:13 +0100

    implemented svm

--- Lab4/SVM.py
+++ Lab4/SVM.py
@@ -1,42 +1,62 @@
 import numpy as np
 from dataclasses import dataclass
-from typing import Literal
+from typing import Literal, Protocol
+from cvxopt import matrix, solvers
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+class Kernel(Protocol):
+    def __call__(self, x1, x2) -> float:
+        pass
 
 
 @dataclass
 class SVMParams:
-    def __init__(self, C: float, max_iter: int, kernel: Literal['linear', 'rbf'], gamma: float, lr: float) -> None:
+    def __init__(self, C: float, kernel: Kernel) -> None:
         self.C = C
-        self.max_iter = max_iter
         self.kernel = kernel
-        self.gamma = gamma
-        self.lr = lr
 
 
 class SVM:
     def __init__(self, params: SVMParams) -> None:
         self.params = params
-        self.support_vectors = None
-        self.support_vector_labels = None
-        self.support_vector_alphas = None
-        self.support_vector_bias = None
-    
-    def linear_kernel(self, x1: np.ndarray, x2: np.ndarray) -> float:
-        return np.dot(x1, x2)
+        self.alpha = None
+        self.w = None
+        self.b = None
 
-    def rbf_kernel(self, x1: np.ndarray, x2: np.ndarray, gamma: float) -> float:
-        return np.exp(-gamma * np.linalg.norm(x1 - x2) ** 2)
+    def _compute_gram_matrix(self, X, y):
+        N = len(y)
+        Q = np.zeros((N, N))
+        for i in range(N):
+            for j in range(N):
+                Q[i, j] = y[i] * y[j] * self.params.kernel(X[i], X[j])
+        return Q
 
-    def compute_kernel(self, X: np.ndarray, kernel: Literal['linear', 'rbf'], gamma: float) -> np.ndarray:
-        n_samples, _ = X.shape
-        K = np.zeros((n_samples, n_samples))
-        for i in range(n_samples):
-            for j in range(n_samples):
-                if kernel == 'linear':
-                    K[i, j] = self.linear_kernel(X[i], X[j])
-                elif kernel == 'rbf':
-                    K[i, j] = self.rbf_kernel(X[i], X[j], gamma)
-        return K
+    def fit(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        N = len(y)
+        P = matrix(self._compute_gram_matrix(X, y))
+        q = matrix(-np.ones(N))
+        G = matrix(np.vstack((-np.eye(N), np.eye(N))))
+        h = matrix(np.hstack((np.zeros(N), np.ones(N) * self.params.C)))
+        A = matrix(y, (1, N), 'd')
+        b = matrix(0.0)
+        solvers.options['show_progress'] = False
+        solution = solvers.qp(P, q, G, h, A, b)
+        alphas = np.ravel(solution['x'])
+        self.w = np.sum((alphas * y)[:, None] * X, axis=0)
+        support_vector_idx = np.where((alphas > 1e-5) & (alphas < self.params.C))[0]
+        self.b = y[support_vector_idx[0]] - np.dot(self.w, X[support_vector_idx[0]])
 
-    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
-        pass
+    def predict(self, X):
+        return np.sign(np.dot(X, self.w) + self.b)
+
+    def evaluate(self, X_test, y_test):
+        y_pred = self.predict(X_test)
+        correct = np.sum(y_pred == y_test)
+
+        cm = confusion_matrix(y_test, y_pred)
+
+        report = classification_report(y_test, y_pred)
+        return correct / len(y_test), cm, report
--- Lab4/main.py
+++ Lab4/main.py
@@ -1,10 +1,28 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
+from SVM import SVM, SVMParams, Kernel
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+
+class LinearKernel(Kernel):
+    def __call__(self, x1, x2):
+        return np.dot(x1, x2)
+
+
+class RBFKernel(Kernel):
+    def __init__(self, gamma):
+        super().__init__()
+        self.gamma = gamma
+
+    def __call__(self, x1, x2):
+        return np.exp(-self.gamma * np.linalg.norm(x1 - x2) ** 2)
 
 
 def load_and_prep_wine_data():
-    df = pd.read_csv('winequality-red.csv', sep=';')
-    df2 = pd.read_csv('winequality-white.csv', sep=';')
+    df = pd.read_csv('./winequality-red.csv', sep=';')
+    df2 = pd.read_csv('./winequality-white.csv', sep=';')
     df = pd.concat([df, df2])
     median = df['quality'].median()
     df['quality'] = df['quality'].apply(lambda x: 1 if x > median else -1)
@@ -17,3 +35,21 @@ def main():
         X = df.drop('quality', axis=1)
         y = df['quality']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        lin_kernel = LinearKernel()
+        rbf_kernel = RBFKernel(gamma=0.1)
+
+        params = SVMParams(C=1, kernel=rbf_kernel)
+        svm_lin = SVM(params)
+        svm_lin.fit(X_train, y_train)
+        accuracy, cm, report = svm_lin.evaluate(X_test, y_test)
+
+        sns.heatmap(cm, annot=True, fmt='d')
+        plt.xlabel('Predicted label')
+        plt.ylabel('True label')
+        plt.show()
+
+        print(f'Accuracy: {accuracy}')
+        print(f'Report: {report}')
+
+
+main()
