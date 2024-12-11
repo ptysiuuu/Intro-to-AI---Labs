f36cd15a3d81712250b3ba7f2a933f4ae3592476
Adam Szostek <xadirpl@gmail.com>
2024-12-11 15:56:00 +0100

    added polynomialkernel

--- Lab4/main.py
+++ Lab4/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from SVM import SVM, SVMParams, Kernel
 import seaborn as sns
 from matplotlib import pyplot as plt
@@ -24,6 +25,15 @@ class RBFKernel(Kernel):
         return np.exp(-self.gamma * np.linalg.norm(x1 - x2) ** 2)
 
 
+class PolynomialKernel:
+    def __init__(self, coef0=1, degree=3):
+        self.coef0 = coef0
+        self.degree = degree
+
+    def __call__(self, x1, x2):
+        return (np.dot(x1, x2) + self.coef0) ** self.degree
+
+
 def load_and_prep_wine_data():
     df = pd.read_csv('./winequality-red.csv', sep=';')
     df2 = pd.read_csv('./winequality-white.csv', sep=';')
@@ -39,10 +49,16 @@ def main():
     if __name__ == "__main__":
         df = load_and_prep_wine_data()
         X = df.drop('quality', axis=1)
+
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
+
         y = df['quality']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
         lin_kernel = LinearKernel()
         rbf_kernel = RBFKernel(gamma=0.1)
+        poly_kernel = PolynomialKernel()
 
         params = SVMParams(C=1, kernel=rbf_kernel)
         svm_lin = SVM(params)
